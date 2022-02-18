d21e98c5b160aea29633b8e83996c65c17ed45ef
Sudip Das <sudip5428@gmail.com>
2022-02-18 16:14:24 +0530

    code commit for models add

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 
@@ -19,5 +21,13 @@ def contact(request):
 
 
 def services(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,datetime=datetime.today())
+        contact.save()
+
     return render(request, 'services.html')
     # return HttpResponse("this is services")
